18e06983d5f8cd698857d9f53fba1bff508c1d9a
Jack Swiggett <jackswiggett@gmail.com>
2016-09-15 12:07:17 -0700

    Add resume data to resumeBuilder.js
    Added bio, education, work, and projects objects in
    resumeBuilder.js. The data within will be used to populate the
    resume. Also added remy.jpg as a profile image.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,111 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+/* Resume data */
+
+var bio = {
+    "name": "Jack Swiggett",
+    "role": "Software Engineer",
+    "contacts": {
+        "mobile": "(123) 456-7890",
+        "email": "jackswiggett@gmail.com",
+        "github": "jackswiggett",
+        "twitter": "@idonthaveatwitter",
+        "location": "Seattle"
+    },
+    "biopic": "images/remy.jpg",
+    "welcomeMessage": "Welcome to my resume!",
+    "skills": [
+        "programming",
+        "music",
+        "rock climbing",
+        "footbag"
+    ]
+}
+
+var education = {
+    "schools": [
+        {
+            "name": "Stanford University",
+            "degree": "B.S.",
+            "dates": "2014-present",
+            "location": "Stanford, CA",
+            "url": "https://www.stanford.edu/",
+            "majors": ["Computer Science"]
+        }
+    ],
+    "onlineCourses": [
+        {
+            "title": "Intro to HTML and CSS",
+            "school": "Udacity",
+            "dates": "September 2016",
+            "url": "http://www.udacity.com/"
+        },
+        {
+            "title": "JavaScript Basics",
+            "school": "Udacity",
+            "dates": "September 2016",
+            "url": "http://www.udacity.com/"
+        }
+    ]
+}
+
+var work = {
+    jobs: [
+        {
+            "employer": "Ineval",
+            "title": "Software intern",
+            "location": "Paris",
+            "dates": "Spring/Summer 2016",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod " +
+                           "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, " +
+                           "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo " +
+                           "consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse " +
+                           "cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat " +
+                           "non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        },
+        {
+            "employer": "Synapse",
+            "title": "Software intern",
+            "location": "Seattle",
+            "dates": "Summer 2015",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod " +
+                           "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, " +
+                           "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo " +
+                           "consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse " +
+                           "cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat " +
+                           "non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        }
+    ]
+}
+
+var projects = {
+    projects: [
+        {
+            "title": "Project 1",
+            "dates": "2013",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod " +
+                           "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, " +
+                           "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo " +
+                           "consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse " +
+                           "cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat " +
+                           "non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            "images": [
+                "http://placekitten.com/200/300",
+                "http://placekitten.com/400/300"
+            ]
+        },
+        {
+            "title": "Project 2",
+            "dates": "2015",
+            "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod " +
+                           "tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, " +
+                           "quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo " +
+                           "consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse " +
+                           "cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat " +
+                           "non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            "images": [
+                "http://placekitten.com/250/250",
+                "http://placekitten.com/450/200",
+                "http://placekitten.com/600/500",
+            ]
+        }
+    ]
+}
